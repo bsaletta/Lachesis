905be76dde56353ad08d163df9965abc3529d48d
Ben <bensaletta@yahoo.com>
2017-11-25 19:45:15 -0800

    Basic Window Gen

---
+++ Lachesis.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Gtk;
+
+namespace Lachesis
+{
+	public class Lachesis{
+		public static void Main(String[] args){
+			Application.Init ();
+			Window mainWindow = new Window ("Lachesis");
+
+			mainWindow.ShowAll ();
+			Application.Run ();
+		}
+	}
+
+}
+
