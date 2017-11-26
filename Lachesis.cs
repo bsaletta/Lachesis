56c82086a3a0332b73cf9dc7e88fb2b8d1fdab15
Ben <bensaletta@yahoo.com>
2017-11-25 22:55:46 -0800

    Menu Bar and Task outline

--- Lachesis.cs
+++ Lachesis.cs
@@ -5,13 +5,8 @@ namespace Lachesis
 {
 	public class Lachesis{
 		public static void Main(String[] args){
-			Application.Init ();
-			Window mainWindow = new Window ("Lachesis");
-
-			mainWindow.ShowAll ();
-			Application.Run ();
-		}
+			new MainWindow ();
 	}
-
+	}
 }
 
---
+++ MainWindow.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Gtk;
+
+namespace Lachesis{
+	public class MainWindow : Window{
+		public MainWindow():base("Lachesis"){
+			Application.Init ();
+			SetDefaultSize (250, 200);
+			DeleteEvent +=Quit;
+			#region --Menu Bar Initialization--
+			MenuBar mb = new MenuBar();
+
+			Menu fileMenu = new Menu ();
+			MenuItem file = new MenuItem ("File");
+			file.Submenu = fileMenu;
+
+			MenuItem quit = new MenuItem ("Quit");
+			quit.Activated += Quit;
+			fileMenu.Append (quit);
+
+			mb.Append (file);
+
+			VBox menuBar = new VBox (false, 2);
+			menuBar.PackStart (mb, false, false, 0);
+
+			Add (menuBar);
+
+			#endregion --End--
+			#region --Project Initializaition Frame--
+
+			//TODO: Build Project Initialization Frame
+			#endregion
+			#region --Design Phase Frame--
+			//TODO: Build Design Phase Frame
+			
+			#endregion
+			#region --Drawing Release Frame--
+			//TODO: Build Drawing Release Frame
+			#endregion
+			#region --Purchasing And Fabrication Frame--
+			//TODO: Build Purchasing and Fabrication Frame
+
+			#endregion
+			ShowAll ();
+			Application.Run ();
+			}
+			void Quit(object sender, EventArgs args){
+				Application.Quit ();
+			}
+		}
+	}
